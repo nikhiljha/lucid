c3053e823b45595691557ba7d47135b4608c499f
Nikhil Jha <hi@nikhiljha.com>
2020-11-15 13:17:25 -0800

    :art: switch argocd to helm chart

--- infra/argocd.py
+++ infra/argocd.py
@@ -1,14 +1,33 @@
 from utils import versions
-from utils import hosted
+from utils import helm
 
 
 namespace = "argocd"
 
 
 def build():
-    t = hosted.YAML(
-        url=f"https://raw.githubusercontent.com/argoproj/argo-cd/v{versions['argocd']}/manifests/install.yaml",
+    metrics = {
+        "enabled": True,
+        "serviceMonitor": {
+            "enabled": True,
+        },
+    }
+
+    # This chart is the non-HA version.
+    t = helm.Chart(
+        repo_name="argo",
+        repo_url="https://argoproj.github.io/argo-helm",
+        chart_name="argo-cd",
         namespace=namespace,
+        version=versions["argocd"],
+        values={
+            # Enable monitoring for all components.
+            # Requires Prometheus Operator
+            "controller": {"metrics": metrics},
+            "dex": {"metrics": metrics},
+            "server": {"metrics": metrics},
+            "repoServer": {"metrics": metrics},
+        },
     )
 
     return t
